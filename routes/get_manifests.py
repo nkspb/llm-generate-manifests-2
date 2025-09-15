b8c98781f835ec9610731c03b5ced43b5b124f8b
Nikolay <nkspb13@gmail.com>
2025-09-15 11:50:41 +0300

    Update get_manifests.py

--- routes/get_manifests.py
+++ routes/get_manifests.py
@@ -1,42 +1,39 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import PlainTextResponse
 from models import QueryRequest
-from core.manifest_flow import start_manifest_flow_from_query
+from core.manifest_engine import start_manifest_flow_from_query
+from core.session_manager import SessionStore
 import logging
+
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 llm = None
 vector_store = None
-sessions = None
-
-logger = logging.getLogger(__name__)
+session_store: SessionStore = None
 
 @router.post("/get_manifests")
 async def get_manifest(request: QueryRequest, fastapi_request: Request):
     query = request.query
+
     client_ip = fastapi_request.client.host if fastapi_request.client else "unknown"
     logger.info(f"[GET_MANIFESTS Request from {client_ip} with query: {query}]")
     
     try:
-        response = start_manifest_flow_from_query(query, vector_store, llm, sessions)
-        if not response:
-            logger.warning("[GET_MANIFESTS] start_manifest_flow_from_query returned None!")
-            return PlainTextResponse(content="Произошла ошибка. Попробуйте другой запрос", status_code=500)
-
-        logger.info(f"[GET_MANIFESTS] ChatResponse: intent={response.intent}, session_id={response.session_id}, reply={response.reply}")
+        response = start_manifest_flow_from_query(query=query, vector_store=vector_store, llm=llm, session_store=session_store)
+    except Exception as e:
+        logger.exception("Error during manifest flow")
         return PlainTextResponse(
-            content=response.reply,
-            headers={
-                "App-Session-Id": response.session_id or "",
-                "App=Intent": response.intent
-            },
+            content="Произошла ошибка при обработке запроса",
+            status_code=500,
             media_type="text/plain"
         )
 
-    except Exception as e:
-        logger.exception(f"[GET_MANIFESTS] Unexpected error: {e}")
-        return PlainTextResponse(
-            content="Произошла ошибка при обработке запроса",
-            status_code=500
-        )
-
+    return PlainTextResponse(
+        content=response.reply,
+        headers={
+            "App-Session-Id": response.session_id or "",
+            "App-Intent": response.intent
+        },
+        media_type="text/plain"
+    )
