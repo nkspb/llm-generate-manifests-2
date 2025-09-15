b1bea4e58fe235488a4dc6068fd109fdf50e7513
Nikolay <nkspb13@gmail.com>
2025-09-15 11:51:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from fastapi.responses import PlainTextResponse
 from typing import Optional, Literal
 from routes import chat, classify, health, get_manifests
 
+
 from pydantic import BaseModel, ValidationError # For validating user's POST request body
 from core.placeholder_engine import extract_placeholders, PLACEHOLDER_TYPES, is_placeholder_valid, fill_placeholders, format_placeholder_list
+from core.session_manager import SessionStore
 
 import logging, os, uuid, json
 
@@ -31,13 +33,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # In-memory session store
-sessions = {}
+session_store = SessionStore()
+# sessions = {}
 
 for module in [chat, classify, get_manifests]:
-    module.sessions = sessions
+    module.session_store = session_store
     module.llm = llm
     if hasattr(module, "vector_store"):
         module.vector_store = vector_store
+
 
 app = FastAPI()
 app.include_router(chat.router)
@@ -45,6 +49,11 @@ app.include_router(classify.router)
 app.include_router(health.router)
 app.include_router(get_manifests.router)
 # If parameter is a Pydantic model, FastAPI reads it from request body
+
+get_manifests.llm = llm
+get_manifests.vector_store = vector_store
+get_manifests.session_store = session_store
+app.include_router(get_manifests.router)
 
 if __name__ == "__main__":
     import uvicorn
